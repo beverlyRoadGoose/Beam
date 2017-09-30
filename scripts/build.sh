04497bd890aff18ff8d9281317f9555f2fc5f21d
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2017-09-30 18:32:46 +0200

    convert scripts from text to bash

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 cmake -DCMAKE_PREFIX_PATH="lib/wxWidgets/build-debug/;lib/sqlite/build-debug/" . && make
---
+++ scripts/clean.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+rm -rf CMakeFiles
+rm -rf cmake_install.cmake
+rm -rf CmakeCache.txt
+rm -rf Makefile
+rm -rf build/Beam
+rm -rf build/episode-tests
+rm -rf build/libCatch.a
+rm -rf build/libTestUtils.a
+rm -rf build/podcast-tests
+rm -rf Testing
+rm -rf CTestTestfile.cmake
