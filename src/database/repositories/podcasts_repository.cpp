4e8f22b1667642a31eed5e1dac6bee4f2f263179
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2017-10-01 23:01:13 +0200

    create podcasts repository

---
+++ src/database/repositories/podcasts_repository.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Oluwatobi Adeyinka on 2017. 10. 01..
+//
+
+#include "podcasts_repository.h"
+
+PodcastsRepository::PodcastsRepository() = default;
+
+PodcastsRepository::~PodcastsRepository() = default;
