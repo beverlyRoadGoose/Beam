238907ce0ca2c39e427458b5e35d20c3cf0c4910
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2017-10-01 18:47:23 +0200

    updaate clean script & create persistence directory at build time

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-rm -rf CMakeFiles
-rm -rf cmake_install.cmake
-rm -rf CmakeCache.txt
-rm -rf Makefile
-rm -rf build/Beam
-rm -rf build/episode-tests
-rm -rf build/libCatch.a
-rm -rf build/libTestUtils.a
-rm -rf build/podcast-tests
 rm -rf Testing
+rm -rf Makefile
+rm -rf CMakeFiles
+rm -rf CmakeCache.txt
+rm -rf cmake_install.cmake
 rm -rf CTestTestfile.cmake
+
+rm -rf build/Beam
+rm -rf build/libCatch.a
+rm -rf build/podcast-tests
+rm -rf build/episode-tests
+rm -rf build/libTestUtils.a
+rm -rf build/database-manager-tests
