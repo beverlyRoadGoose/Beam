5e0fd77c2354c67f6e49d33af19f4820498a4f91
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2017-10-02 03:44:21 +0200

    fix failing travis build

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-export CXX=g++
-export CC=gcc
 mkdir lib && cd lib && mkdir catch && cd catch && touch catch.hpp && touch catch.cpp
 curl -L https://github.com/philsquared/Catch/releases/download/v1.10.0/catch.hpp >> catch.hpp
 cd .. && curl -L https://sqlite.org/2017/sqlite-autoconf-3200100.tar.gz | tar zx
