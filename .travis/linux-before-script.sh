b67a650c511834bbb8b175c984cc01d4c57647c1
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2017-10-02 03:18:48 +0200

    fix attempt for last ci test

---
+++ .travis/linux-before-script.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo apt-get install libgtk-3-dev build-essential checkinstall
+sudo apt-get update
--- scripts/travis-linux-before-script.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get install build-essential
-sudo apt-get update
