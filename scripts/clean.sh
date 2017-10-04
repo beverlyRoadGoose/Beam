927afb9d7484bfe4d98149c68618ced64334712d
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2017-10-04 17:58:01 +0200

    refactor CMakeLists.txt

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# files
 rm -rf Testing
 rm -rf Makefile
 rm -rf CMakeFiles
@@ -7,11 +8,17 @@ rm -rf CmakeCache.txt
 rm -rf cmake_install.cmake
 rm -rf CTestTestfile.cmake
 
+# executables
 rm -rf build/Beam
-rm -rf build/libCatch.a
 rm -rf build/podcast-tests
 rm -rf build/episode-tests
 rm -rf build/episodes-repository-tests
 rm -rf build/podcasts-repository-tests
-rm -rf build/libTestUtils.a
 rm -rf build/database-manager-tests
+
+# libraries
+rm -rf build/libCatch.a
+rm -rf build/libbeam-ui.a
+rm -rf build/libTestUtils.a
+rm -rf build/libbeam-entities.a
+rm -rf build/libbeam-database.a
